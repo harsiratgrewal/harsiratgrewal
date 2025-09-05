9872294316e8d1b43ec97493b9b96479c0bd5084
Harsirat <harsiratgrewal.hg@gmail.com>
2025-09-05 12:17:08 -0400

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Harsirat Grewal",
   description: "Personal website of Harsirat Grewal",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
